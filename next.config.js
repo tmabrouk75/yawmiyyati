d1cbe1c8295154e82e5137e8f0e14038cd4a9119
tmabrouk75 <t.f.mabrouk@gmail.com>
2026-05-29 15:55:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,6 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: {
     domains: ['blob.vercel-storage.com'],
